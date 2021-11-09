c2f7172049de35cd52e9d435099830689a2778ec
Rafael <brandaofx@gmail.com>
2021-11-09 16:35:55 -0300

    first commit

---
+++ removendo-nota.js
@@ -0,0 +1,9 @@
+const nota = [10,7,8,5,10]
+nota.pop()
+console.log(nota)
+
+media = (nota[0] + nota[1] + nota[2] + nota[3] / nota.length)
+
+console.log(`a media é ${media}`)
+
+console.log("nota")
