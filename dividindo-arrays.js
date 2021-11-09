35232a8794982cc478926a839a0784bf2528a0c7
Rafael <brandaofx@gmail.com>
2021-11-09 16:52:30 -0300

    dividindo-arrays.js

---
+++ dividindo-arrays.js
@@ -0,0 +1,7 @@
+const nomes = ['Rafael','Leandro','Marta','Claudio','Cintia','Carol','Jairo','Carlao','Santoro','Evelin','Adriano']
+
+const sala1 = nomes.slice(0,nomes.length/2)
+const sala2 = nomes.slice(nomes.length/2)
+
+console.log(`alunosda nossa sala 1 ${sala1}`)
+console.log(`alunosda nossa sala 2 ${sala2}`)
